036b618121dc4cca4e8c9f6af7fb7a931dae6b72
Matthew Smith <matthewsssmith1@gmail.com>
2020-07-11 18:00:49 -0400

    moved content-panel into a wrapper to allow for its children (the draggable region on the left side) to have absolute sizing

--- src/renderer/content_panel/content_panel.tsx
+++ src/renderer/content_panel/content_panel.tsx
@@ -2,6 +2,12 @@ import * as React from 'react'
 
 export default class ContentPanel extends React.Component {
    render(): JSX.Element {
-      return <div className="content-panel" />
+      return (
+         <div className="content-panel-wrapper">
+            <div className="content-panel">
+               <div className="content-panel__draggable-edge" />
+            </div>
+         </div>
+      )
    }
 }
