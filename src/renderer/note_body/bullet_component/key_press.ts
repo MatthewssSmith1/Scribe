21501d85504c593c0411fc4c1a6f491ce052e459
Matthew Smith <matthewsssmith1@gmail.com>
2020-07-17 16:36:03 -0400

    consolidated key_press.ts into one method

--- src/renderer/note_body/bullet_component/key_press.ts
+++ src/renderer/note_body/bullet_component/key_press.ts
@@ -1,203 +1,185 @@
 import * as React from 'react'
 import Bullet from '@main/bullet'
 import { ContextStateType, ContextDispatchType } from '@renderer/state/context'
-import { addFocusBulletToIndex, removeFocusedBullet, LinkMenuState, showLinkMenu } from '@renderer/state/context_actions'
+import { LinkMenuState, showLinkMenu } from '@renderer/state/context_actions'
 import WorkspaceManager from '@main/workspace_manager'
 
-export default function handleKeyPress(state: ContextStateType, dispatch: ContextDispatchType, e: React.KeyboardEvent<HTMLDivElement>, blt: Bullet) {
+type KeyEvent = React.KeyboardEvent<HTMLDivElement>
+
+export default function handleKeyPress(state: ContextStateType, dispatch: ContextDispatchType, evt: KeyEvent, bullet: Bullet) {
    //TODO make enter with selection not collapsed start linking process
 
-   var sel = window.getSelection()
+   //TODO handle focused bullet list on enter/backspace and move left/right
 
-   handleEnter(state, dispatch, e, blt, sel)
-   handleBackspace(state, dispatch, e, blt, sel)
-   handleBracketsAndTab(state, e, blt)
-   handleArrows(state, e, blt)
-}
+   var selection = window.getSelection()
 
-function handleEnter(
-   state: ContextStateType,
-   dispatch: ContextDispatchType,
-   evt: React.KeyboardEvent<HTMLDivElement>,
-   bullet: Bullet,
-   selection: Selection
-): boolean {
-   if (evt.key != 'Enter') return false
+   const rebuildAndPreventDefault = () => {
+      //rebuild tree
+      state.noteBody.rootBullet.updateComponent()
 
-   // if any part of the selected text is in a span link, do nothing
-   if (
-      selection.anchorNode.parentElement.tagName == 'SPAN' ||
-      selection.focusNode.parentElement.tagName == 'SPAN' ||
-      selection.getRangeAt(0).cloneContents().querySelector('span') != null
-   ) {
+      //stop normal key behavior
       evt.preventDefault()
-      return
    }
 
-   var getAnchorIndex = (): number => {
-      var anchorIndex: number = selection.anchorOffset
+   const handleEnter = () => {
+      if (evt.key != 'Enter') return
 
-      var node: Node = selection.anchorNode
+      //prevent default enter behavior no matter what
+      evt.preventDefault()
 
-      //traverse up the DOM until at an element that is a child of the contenteditable div
-      while (node.parentElement.tagName != 'DIV') {
-         //add the length of the opening tag to the caretPos
-         anchorIndex += node.parentElement.outerHTML.indexOf('>') + 1
+      // if any part of the selected text is in a span link, do nothing
+      if (
+         selection.anchorNode.parentElement.tagName == 'SPAN' ||
+         selection.focusNode.parentElement.tagName == 'SPAN' ||
+         selection.getRangeAt(0).cloneContents().querySelector('span') != null
+      )
+         return
 
-         node = node.parentElement
-      }
+      //TODO move this behavior to bullet
+      var getAnchorIndex = (): number => {
+         var anchorIndex: number = selection.anchorOffset
 
-      //traverse across the DOM for each sibling before the node from the previous while loop
-      while (node.previousSibling != null) {
-         node = node.previousSibling
+         var node: Node = selection.anchorNode
 
-         //add the length of the node (either the textContent of text nodes or the html string of elements) to the caret pos
-         if (node.nodeType == Node.TEXT_NODE) {
-            anchorIndex += node.textContent.length
-         } else {
-            anchorIndex += (node as HTMLElement).outerHTML.length
+         //traverse up the DOM until at an element that is a child of the contenteditable div
+         while (node.parentElement.tagName != 'DIV') {
+            //add the length of the opening tag to the caretPos
+            anchorIndex += node.parentElement.outerHTML.indexOf('>') + 1
+
+            node = node.parentElement
          }
-      }
 
-      return anchorIndex
-   }
+         //traverse across the DOM for each sibling before the node from the previous while loop
+         while (node.previousSibling != null) {
+            node = node.previousSibling
 
-   var getFocusIndex = (): number => {
-      var focusIndex: number = selection.focusOffset
-
-      var node: Node = selection.focusNode
-
-      //traverse up the DOM until at an element that is a child of the contenteditable div
-      while (node.parentElement.tagName != 'DIV') {
-         //add the length of the opening tag to the caretPos
-         focusIndex += node.parentElement.outerHTML.indexOf('>') + 1
-
-         node = node.parentElement
-      }
-
-      //traverse across the DOM for each sibling before the node from the previous while loop
-      while (node.previousSibling != null) {
-         node = node.previousSibling
-
-         //add the length of the node (either the textContent of text nodes or the html string of elements) to the caret pos
-         if (node.nodeType == Node.TEXT_NODE) {
-            focusIndex += node.textContent.length
-         } else {
-            focusIndex += (node as HTMLElement).outerHTML.length
+            //add the length of the node (either the textContent of text nodes or the html string of elements) to the caret pos
+            if (node.nodeType == Node.TEXT_NODE) {
+               anchorIndex += node.textContent.length
+            } else {
+               anchorIndex += (node as HTMLElement).outerHTML.length
+            }
          }
+
+         return anchorIndex
       }
 
-      return focusIndex
-   }
+      var getFocusIndex = (): number => {
+         var focusIndex: number = selection.focusOffset
 
-   var selectionStartIndex = getAnchorIndex()
-   var selectionEndIndex = getFocusIndex()
+         var node: Node = selection.focusNode
 
-   //ensure start index is before end index
-   if (selectionStartIndex > selectionEndIndex) {
-      var temp = selectionEndIndex
-      selectionEndIndex = selectionStartIndex
-      selectionStartIndex = temp
-   }
+         //traverse up the DOM until at an element that is a child of the contenteditable div
+         while (node.parentElement.tagName != 'DIV') {
+            //add the length of the opening tag to the caretPos
+            focusIndex += node.parentElement.outerHTML.indexOf('>') + 1
 
-   if (selection.isCollapsed) {
-      var textBeforeCaret = bullet.text.substring(0, selectionStartIndex)
-      var textAfterCaret = bullet.text.substring(selectionStartIndex).trim() //? the trim should be unnecessary
+            node = node.parentElement
+         }
 
-      bullet.addSibling(-1, new Bullet(textBeforeCaret))
-      bullet.text = textAfterCaret
+         //traverse across the DOM for each sibling before the node from the previous while loop
+         while (node.previousSibling != null) {
+            node = node.previousSibling
 
-      bullet.selectComponent(0)
-      bullet.parent.updateComponent()
+            //add the length of the node (either the textContent of text nodes or the html string of elements) to the caret pos
+            if (node.nodeType == Node.TEXT_NODE) {
+               focusIndex += node.textContent.length
+            } else {
+               focusIndex += (node as HTMLElement).outerHTML.length
+            }
+         }
 
-      bullet.updateComponent()
-   } else {
-      var selectedText = selection.toString()
-      var selectionRect = selection.getRangeAt(0).getBoundingClientRect()
-      var linkMenuState: LinkMenuState = {
-         isHidden: false,
-         viewportPos: [selectionRect.left, selectionRect.bottom],
-         bulletWithSelection: bullet,
-         selectionBounds: [selectionStartIndex, selectionEndIndex],
-         selectedText,
-         suggestedLinks: WorkspaceManager.getSuggestedLinks(selectedText),
-      }
-      dispatch(showLinkMenu(linkMenuState))
-   }
-
-   evt.preventDefault()
-}
-
-function handleBackspace(
-   state: ContextStateType,
-   dispatch: ContextDispatchType,
-   evt: React.KeyboardEvent<HTMLDivElement>,
-   bullet: Bullet,
-   selection: Selection
-) {
-   if (evt.key != 'Backspace' || !selection.isCollapsed || selection.anchorOffset != 0) return
-
-   if (bullet.isFirstSibling) {
-      if (bullet.parent.isRoot) return
-      //move siblings following bullet to be children of bullet
-      var siblingsAfterBullet = bullet.parent.removeChildren(1)
-      bullet.addChildrenToEnd(siblingsAfterBullet)
-
-      //move bullet to be the sibling after its old parent
-      bullet.parent.addSibling(1, bullet)
-
-      var sibFocusedBulletsIndex = state.noteBody.focusedBullets.indexOf(bullet.siblingBefore)
-      if (sibFocusedBulletsIndex != -1) {
-         dispatch(addFocusBulletToIndex(bullet, sibFocusedBulletsIndex + 1))
+         return focusIndex
       }
 
-      bullet.selectComponent(selection.anchorOffset)
-      state.noteBody.rootBullet.updateComponent()
-   } else if (bullet.siblingBefore.childCount == 0) {
-      //store length of the text in the bullet before (set cursor pos here later)
-      var sibling = bullet.siblingBefore
-      bullet.selectComponent(sibling.text.length)
+      var selectionStartIndex = getAnchorIndex()
+      var selectionEndIndex = getFocusIndex()
 
-      //concat the two bullets
-      bullet.text = sibling.remove().text + bullet.text
+      //ensure start index is before end index
+      if (selectionStartIndex > selectionEndIndex) {
+         var temp = selectionEndIndex
+         selectionEndIndex = selectionStartIndex
+         selectionStartIndex = temp
+      }
 
-      state.noteBody.rootBullet.updateComponent()
+      if (selection.isCollapsed) {
+         var textBeforeCaret = bullet.text.substring(0, selectionStartIndex)
+         var textAfterCaret = bullet.text.substring(selectionStartIndex).trim() //? the trim should be unnecessary
+
+         bullet.addSibling(-1, new Bullet(textBeforeCaret))
+         bullet.text = textAfterCaret
+
+         bullet.selectComponent(0)
+         bullet.parent.updateComponent()
+
+         bullet.updateComponent()
+      } else {
+         var selectedText = selection.toString()
+         var selectionRect = selection.getRangeAt(0).getBoundingClientRect()
+         var linkMenuState: LinkMenuState = {
+            isHidden: false,
+            viewportPos: [selectionRect.left, selectionRect.bottom],
+            bulletWithSelection: bullet,
+            selectionBounds: [selectionStartIndex, selectionEndIndex],
+            selectedText,
+            suggestedLinks: WorkspaceManager.getSuggestedLinks(selectedText),
+         }
+         dispatch(showLinkMenu(linkMenuState))
+      }
    }
 
-   evt.preventDefault()
-}
+   const handleBackspace = () => {
+      if (evt.key != 'Backspace' || !selection.isCollapsed || selection.anchorOffset != 0) return
 
-function handleBracketsAndTab(state: ContextStateType, evt: React.KeyboardEvent<HTMLDivElement>, bullet: Bullet) {
-   const lBracketCtrl = evt.key == '[' && evt.ctrlKey
-   const rBracketCtrl = evt.key == ']' && evt.ctrlKey
+      if (bullet.isFirstSibling) {
+         if (!bullet.hasGrandParent) return
 
-   if (lBracketCtrl) bullet.moveLeft()
-   else if (!bullet.isFirstSibling && (rBracketCtrl || evt.key == 'Tab')) bullet.moveRight()
-   else return
+         bullet.moveLeft()
 
-   // TODO select the bullet in the same place it was before
+         bullet.selectComponent(selection.anchorOffset)
+      } else if (bullet.siblingBefore.childCount == 0) {
+         //store length of the text in the bullet before (set cursor pos here later)
+         var sibling = bullet.siblingBefore
+         bullet.selectComponent(sibling.text.length)
 
-   //rebuild tree
-   state.noteBody.rootBullet.updateComponent()
+         //concat the two bullets
+         bullet.text = sibling.remove().text + bullet.text
+      }
 
-   //stop normal key behavior
-   evt.preventDefault()
-}
+      rebuildAndPreventDefault()
+   }
 
-function handleArrows(state: ContextStateType, evt: React.KeyboardEvent<HTMLDivElement>, bullet: Bullet) {
-   if (!evt.altKey && !evt.ctrlKey) return
+   const handleBracketsAndTab = () => {
+      const lBracketCtrl = evt.key == '[' && evt.ctrlKey
+      const rBracketCtrl = evt.key == ']' && evt.ctrlKey
 
-   var otherBullet: Bullet
+      if (bullet.hasGrandParent && lBracketCtrl) bullet.moveLeft()
+      else if (!bullet.isFirstSibling && (rBracketCtrl || evt.key == 'Tab')) bullet.moveRight()
+      else return
 
-   if (evt.key == 'ArrowUp') otherBullet = bullet.bulletBefore
-   else if (evt.key == 'ArrowDown') otherBullet = bullet.bulletAfter
-   else return
+      // TODO select the bullet in the same place it was before
 
-   if (evt.altKey) bullet.swapTextWith(otherBullet)
+      rebuildAndPreventDefault()
+   }
 
-   //rebuild tree
-   state.noteBody.rootBullet.updateComponent()
+   const handleArrows = () => {
+      if (!evt.altKey && !evt.ctrlKey) return
 
-   //stop normal key behavior
-   evt.preventDefault()
+      var otherBullet: Bullet
+
+      if (evt.key == 'ArrowUp') otherBullet = bullet.bulletBefore
+      else if (evt.key == 'ArrowDown') otherBullet = bullet.bulletAfter
+      else return
+
+      if (evt.altKey) bullet.swapTextWith(otherBullet)
+
+      // TODO select the bullet in the same place it was before
+
+      rebuildAndPreventDefault()
+   }
+
+   handleEnter()
+   handleBackspace()
+   handleBracketsAndTab()
+   handleArrows()
 }
